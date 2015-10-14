386990b2ffca8ca89f08038c6019c0e1b261b468
george <goblin_george@gmail.com>
2015-10-14 14:39:13 +0800

    first commit

---
+++ parse.py
@@ -0,0 +1,84 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright © 2015 george 
+#
+# Distributed under terms of the MIT license.
+__all__ = ["parser", "pretty"]
+import copy
+
+def find_brackets_pair(tokens):
+    left = 0
+    idx = 0
+    for token in tokens:
+        if token == "(":
+            left += 1
+        if token == ")":
+            left -= 1
+        idx += 1
+        if left == -1:
+            return idx
+
+## var count
+var_count = 0
+def parse_tokens(tokens, parents=[]):
+    global var_count
+    result = []
+    idx = 0
+    while tokens:
+        curr = tokens[0]
+        tokens = tokens[1:]
+
+        ## lambda pattern
+        if curr.startswith("\\"):
+            ## extra var name 
+            var = curr.replace("\\", "")
+
+            ## replace var name 
+            var_count += 1
+            lambda_pattern = parents + [(var, var_count)]
+
+            result.append(["lam", var_count, parse_tokens(tokens, lambda_pattern)])
+            break
+
+        ## () pattern
+        elif curr == '(':
+            pair = find_brackets_pair(tokens)
+            result.append(parse_tokens(tokens[:pair-1]), parents)
+            tokens = tokens[pair:]
+
+        ## var pattern
+        else:
+            var = dict(parents).get(curr, False)
+            if not var:
+                var_count += 1
+                var = var_count
+            result.append(["var", var])
+    
+    ## merge 
+    while len(result) > 1:
+        result = [["app", result[0], result[1]]] + result[2:]
+
+    return result[0]
+
+
+def parser(lambda_query):
+    tokens = lambda_query.replace("(", " ( ").replace(")", " ) ").replace("\\", " \\").strip().split()
+    return parse_tokens(tokens)
+
+
+def pretty(data):           
+    lambda_type = data[0]   
+    lambda_info = data[1:]  
+    if lambda_type == 'app':
+        return "(%s)(%s)" % (pretty(lambda_info[0]), pretty(lambda_info[1]))
+    elif lambda_type == 'var':
+        return lambda_info[0]
+    elif lambda_type == 'lam':
+        return "\%s %s" % (lambda_info[0], pretty(lambda_info[1]))                                                                                                                                                   
+
+if __name__ == '__main__':
+    result = parser(r"(\a\b a)b c")
+    print result
+    print pretty(result)
+
