5ece9dcd5f542de4c3acff279701ac56e653a0c8
george <goblin_george@gmail.com>
2015-10-15 18:54:30 +0800

    fix parser

--- parse.py
+++ parse.py
@@ -4,7 +4,7 @@
 # Copyright © 2015 george
 #
 # Distributed under terms of the MIT license.
-__all__ = ["parser", "pretty", "weak_normal_form"]
+__all__ = ["parser", "pretty", "weak_normal_form", "cover_var"]
 import copy
 
 def find_brackets_pair(tokens):
@@ -77,27 +77,30 @@ def cover_var(lambda_tokens, var, value):
         lambda_tokens[2] = cover_var(lambda_tokens[2], var, value)
     return lambda_tokens
 
-def weak_normal_form(lambda_tokens):
+def new_var():
+    new_var.idx += 1
+    return "@{}".format(new_var.idx)
+new_var.idx = 0
 
-    def trans_var_name(lambda_tokens, parents=[]):
-        if lambda_tokens[0] == "app":
-            trans_var_name(lambda_tokens[1], parents)
-            trans_var_name(lambda_tokens[2], parents)
-        elif lambda_tokens[0] == "lam":
-            trans_var_name.var_idx += 1
-            var_name = "@{}".format(trans_var_name.var_idx)
-            lambda_tokens[2] = trans_var_name(lambda_tokens[2], parents + [(lambda_tokens[1], "@{}".format(trans_var_name.var_idx))])
-            lambda_tokens[1] = var_name
-        elif lambda_tokens[0] == "var":
-            var_name = dict(parents).get(lambda_tokens[1], False)
-            if not var_name:
-                trans_var_name.var_idx+=1 
-                var_name = "@{}".format(trans_var_name.var_idx)
-            lambda_tokens[1] = var_name
-        return lambda_tokens
-    trans_var_name.var_idx = 0
+def trans_var_name(lambda_tokens, env=[], exclude=[]):
+    if lambda_tokens[0] == "app":
+        trans_var_name(lambda_tokens[1], env)
+        trans_var_name(lambda_tokens[2], env)
+    elif lambda_tokens[0] == "lam":
+        var_name = new_var()
+        lambda_tokens[2] = trans_var_name(lambda_tokens[2], env + [(lambda_tokens[1], var_name)])
+        lambda_tokens[1] = var_name
+    elif lambda_tokens[0] == "var":
+        var_name = dict(env).get(lambda_tokens[1], False)
+        if not var_name and lambda_tokens[1] in exclude:
+            var_name = lambda_tokens[1]
+        if not var_name:
+            var_name = new_var()
+        lambda_tokens[1] = var_name
+    return lambda_tokens
 
-    
+
+def weak_normal_form(lambda_tokens, env=[], exclude=[]):
     var_tmp = []
     while lambda_tokens[0] == "app" or var_tmp:
 
@@ -106,8 +109,11 @@ def weak_normal_form(lambda_tokens):
             lambda_tokens = lambda_tokens[1]
         elif lambda_tokens[0] == "lam":
             var_name = lambda_tokens[1]
+            new_env = env
+            if var_name not in dict(env):
+                new_env += [(var_name, new_var())]
             curr = var_tmp.pop()
-            var = trans_var_name(copy.deepcopy(curr))
+            var = trans_var_name(copy.deepcopy(curr), new_env, exclude)
 
             lambda_tokens = lambda_tokens[2]
             cover_var(lambda_tokens, var_name, var)
@@ -122,28 +128,27 @@ def weak_normal_form(lambda_tokens):
     return lambda_tokens
 
         
-def normal_form(lambda_tokens):
+def normal_form(lambda_tokens, env=[], exclude=[]):
     if lambda_tokens[0] == "lam":
-        lambda_tokens = ["lam", lambda_tokens[1], normal_form(lambda_tokens[2])]
+        lambda_tokens = ["lam", lambda_tokens[1], normal_form(lambda_tokens[2], env, exclude+[lambda_tokens[1]])]
     elif lambda_tokens[0] == "app":
-        lambda_tokens = weak_normal_form(lambda_tokens)
+        lambda_tokens = weak_normal_form(lambda_tokens, env, exclude)
         if lambda_tokens[0] == "app":
-            lambda_tokens[1] = normal_form(lambda_tokens[1])
-            lambda_tokens[2] = normal_form(lambda_tokens[2])
+            lambda_tokens[1] = normal_form(lambda_tokens[1], env, exclude)
+            lambda_tokens[2] = normal_form(lambda_tokens[2], env, exclude)
         elif lambda_tokens[0] == "lam":
-            lambda_tokens = normal_form(lambda_tokens)
+            lambda_tokens = normal_form(lambda_tokens, env, exclude)
 
     return lambda_tokens
 
 if __name__ == '__main__':
     result = parser(r"""
-    (\a \b \c a c c )(\a \b (a b) (\a \b b))(\a \b b)(\a \b a)
-
+            (\true(\false(\not not true)(\p \a \b p b a))(\a \b b))(\a \b a)
             """)
 
     result1 = pretty(result)
     parser(result1)
 
-    print weak_normal_form(result)
+    print normal_form(result)
 
 
